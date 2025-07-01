32c500402b7b7e1d136785d55f06b502223718b3
Ozzy <tacojrjr@outlook.com>
2025-07-01 14:42:00 -0300

    Elevator lights

--- Scripts/Guns/casing.gd
+++ Scripts/Guns/casing.gd
@@ -1,20 +1,14 @@
 extends RigidBody3D
 
 func _ready() -> void:
-	print(global_position)
-	print($Marker3D.global_position)
 	if global_position.x > $Marker3D.global_position.x:
 		linear_velocity.x = global_position.x - $Marker3D.global_position.x
-		print("first")
 	else:
 		linear_velocity.x = $Marker3D.global_position.x - global_position.x
-		print("second")
 	if global_position.z > $Marker3D.global_position.z:
 		linear_velocity.z = global_position.z - $Marker3D.global_position.z
-		print("third")
 	else:
 		linear_velocity.z = $Marker3D.global_position.z - global_position.z
-		print("last")
 	if randi_range(0, 1) == 0:
 		constant_torque.x = 0.01
 		constant_torque.y = 0.01
