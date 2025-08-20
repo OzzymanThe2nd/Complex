5d04a2c4c605e2ab9a35c4ab08a81a711d836475
Ozzy <tacojrjr@outlook.com>
2025-08-20 07:20:22 -0300

    Shotgun model

--- Scripts/Entities/enemy_pistol.gd
+++ Scripts/Entities/enemy_pistol.gd
@@ -1,7 +1,10 @@
 extends enemy_base
 
 # Called when the node enters the scene tree for the first time.
-
+func _ready() -> void:
+	#$hoboattempt/rig/Skeleton3D/PhysicalBoneSimulator3D.active = true
+	pass
 
 func _on_timer_timeout() -> void:
-	shoot()
+	$AnimationPlayer.active = false
+	$hoboattempt/rig/Skeleton3D/PhysicalBoneSimulator3D.physical_bones_start_simulation()
