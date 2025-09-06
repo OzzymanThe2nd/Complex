34cefa8f6bb3226a6dc049eb5251123e36db0128
Ozzy <tacojrjr@outlook.com>
2025-09-06 06:50:10 -0300

    Finally functional gameready hobo

--- Scripts/Entities/enemy_pistol.gd
+++ Scripts/Entities/enemy_pistol.gd
@@ -7,7 +7,7 @@ func _ready() -> void:
 
 func _on_timer_timeout() -> void:
 	$AnimationPlayer.active = false
-	$hobo_goodrig/Skeleton3D/PhysicalBoneSimulator3D.physical_bones_start_simulation()
+	#$hobo_goodrig/Skeleton3D/PhysicalBoneSimulator3D.physical_bones_start_simulation()
 	$RagdollTimer.start()
 
 func _on_ragdolltimer_timeout() -> void:
