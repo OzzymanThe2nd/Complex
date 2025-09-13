622add575110f3e30e12976908f18a469ce53f99
Ozzy <tacojrjr@outlook.com>
2025-09-13 11:23:08 -0300

    Skeleton physical set up

--- Scripts/Entities/enemy_pistol.gd
+++ Scripts/Entities/enemy_pistol.gd
@@ -3,8 +3,8 @@ extends enemy_base
 # Called when the node enters the scene tree for the first time.
 
 func _on_timer_timeout() -> void:
-	shoot()
-
+	$AnimationPlayer.active = false
+	$hobogameready/Skeleton3D/PhysicalBoneSimulator3D.physical_bones_start_simulation()
 func _on_ragdolltimer_timeout() -> void:
 	#$hobo_goodrig/Skeleton3D/PhysicalBoneSimulator3D.influence = 0.1
 	pass
