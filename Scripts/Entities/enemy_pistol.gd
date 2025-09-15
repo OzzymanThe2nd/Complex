996d46b37c81ba5d1efb111ff33034227a30addc
Oscar Hawkins <tacojrjr@outlook.com>
2025-09-15 10:27:59 -0300

    Ragdoll updates

--- Scripts/Entities/enemy_pistol.gd
+++ Scripts/Entities/enemy_pistol.gd
@@ -9,10 +9,15 @@ func ragdoll():
 	var velocity_dir = global_basis * Vector3.MODEL_REAR
 	$AnimationPlayer.active = false
 	$hobogameready/Skeleton3D/PhysicalBoneSimulator3D.physical_bones_start_simulation()
-	$"hobogameready/Skeleton3D/PhysicalBoneSimulator3D/Physical Bone Chest".linear_velocity = (velocity_dir) * 2
+	$"hobogameready/Skeleton3D/PhysicalBoneSimulator3D/Physical Bone Chest".linear_velocity = (velocity_dir) * 3
 	$"hobogameready/Skeleton3D/PhysicalBoneSimulator3D/Physical Bone Spine".linear_velocity = (velocity_dir) * 2
 	$"hobogameready/Skeleton3D/PhysicalBoneSimulator3D/Physical Bone Hip".linear_velocity = (velocity_dir) * 2
-	$"hobogameready/Skeleton3D/PhysicalBoneSimulator3D/Physical Bone Neck".linear_velocity = (velocity_dir) * 3
+	$"hobogameready/Skeleton3D/PhysicalBoneSimulator3D/Physical Bone Neck".linear_velocity = (velocity_dir) * 4
+	$RagdollTimer.start()
+
 func _on_ragdolltimer_timeout() -> void:
-	#$hobo_goodrig/Skeleton3D/PhysicalBoneSimulator3D.influence = 0.1
-	pass
+	$"hobogameready/Skeleton3D/PhysicalBoneSimulator3D/Physical Bone Chest".linear_velocity = Vector3(0,0,0)
+	$"hobogameready/Skeleton3D/PhysicalBoneSimulator3D/Physical Bone Spine".linear_velocity = Vector3(0,0,0)
+	$"hobogameready/Skeleton3D/PhysicalBoneSimulator3D/Physical Bone Hip".linear_velocity = Vector3(0,0,0)
+	$"hobogameready/Skeleton3D/PhysicalBoneSimulator3D/Physical Bone Neck".linear_velocity = Vector3(0,0,0)
+	$"hobogameready/Skeleton3D/PhysicalBoneSimulator3D/Physical Bone Neck".angular_velocity.x = 0
