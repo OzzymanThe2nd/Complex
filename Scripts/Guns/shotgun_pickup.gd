fb73ad59027b570bf18d94c6a86c1e138e7c7713
Ozzy <tacojrjr@outlook.com>
2025-11-02 07:13:39 -0400

    Shotgun pickup

--- Scripts/Guns/shotgun_pickup.gd
+++ Scripts/Guns/shotgun_pickup.gd
@@ -2,10 +2,10 @@ extends Node3D
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.name == "Player":
-		PlayerStatus.rifle_unlocked = true
+		PlayerStatus.shotgun_unlocked = true
 		if body.current_weapon != null:
-			if not body.current_weapon.is_in_group("rifle"): body.queued = "rifle"
+			if not body.current_weapon.is_in_group("shotgun"): body.queued = "shotgun"
 			body.current_weapon.unequip()
 		else:
-			body.equip_rifle()
+			body.equip_shotgun()
 		queue_free()
